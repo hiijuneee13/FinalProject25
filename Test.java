823d834e2457f9fcbd07d2ce898bd328a97e0c7a
fami0110 <masandofami@gmail.com>
2025-09-25 09:44:21 +0700

    added update by fami

--- Test.java
+++ Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Halo, ini baris kode pertama");
         System.out.println("Aubin Menulis kode di baris kedua. HIDUP MAHASISWA!");
+        System.out.println("Fami join the server! HIDUP JOK");
     }
 }
