6ab364fe14f40717928446f94d5e295d66c43839
dancelhal2000 <aobinaobin15@gmail.com>
2025-09-25 09:42:48 +0700

    Added updates by aubin

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("Halo, ini baris kode pertama");
+        System.out.println("Aubin Menulis kode di baris kedua. HIDUP MAHASISWA!");
     }
 }
