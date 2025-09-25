655c2d7114ba9e23483f0cdc4246562962bffd1d
fami0110 <masandofami@gmail.com>
2025-09-25 09:53:53 +0700

    nyobak conflict

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Halo, ini baris kode pertama");
         System.out.println("Aubin Menulis kode di baris kedua. HIDUP MAHASISWA!");
-        System.out.println("Fami join the server! HIDUP JOK");
+        System.out.println("Fami join the server! HIDUP JOK-");
+        System.out.println("Fafa are online now! HIDUP BLOND-");
     }
 }
