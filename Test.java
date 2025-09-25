76dddb161d1d4ebe575693341f17db417683ba85
rafaachyana <rafaachyana778@gmail.com>
2025-09-25 10:00:07 +0700

    ganti

--- Test.java
+++ Test.java
@@ -3,6 +3,6 @@ public class Test {
         System.out.println("Halo, ini baris kode pertama");
         System.out.println("Aubin Menulis kode di baris kedua. HIDUP MAHASISWA!");
         System.out.println("Fami join the server! HIDUP JOK-");
-        System.out.println("Rafa join the trend! HIDUP BLONDE!!!!!!!");
+        System.out.println("Rafa join the trend! HIDUP YANG INGIN HIDUP!!!!!!!");
     }
 }
